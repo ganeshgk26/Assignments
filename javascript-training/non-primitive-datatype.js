7b41bd2f8829a443ab6984ee6b37a2f6789357cc
Ganeshkumar <ganeshkumar.26@outlook.com>
2026-01-14 00:45:54 +0530

    Final assignment

---
+++ javascript-training/non-primitive-datatype.js
@@ -0,0 +1,34 @@
+// Object: Represents a collection of key value pairs
+let detailsOfPersons = {
+    name: "Ganesh",
+    age: 30,
+    havingVisa: true,
+    address:[{
+        city: 'Hyd',
+        state: 'TS',
+        country: "India",
+    },
+   {
+     city: "NY",
+     state: "NY",
+     country: "USA",
+    }]
+}
+
+console.log(detailsOfPersons.age);
+console.log(detailsOfPersons.address[0].country);
+
+
+// Array: list of values
+let empName = ["Ganesh", "Kumar", "Bala"]
+console.log(empName[0]);
+
+let empNameAndId = ["Ganesh", 26, "Kumar", 27, "Bala",28]
+console.log(empNameAndId[0]);
+console.log(empNameAndId[1]);
+
+//
+
+
+
+
