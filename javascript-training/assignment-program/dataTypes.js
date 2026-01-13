8291903bb3e96eea868032797ebd9750d09f493e
Ganeshkumar <ganeshkumar.26@outlook.com>
2026-01-13 08:11:53 +0530

    datatypes

---
+++ javascript-training/assignment-program/dataTypes.js
@@ -0,0 +1,30 @@
+//1. Number
+
+let number=45;
+console.log(number + 20);
+console.log(number);
+let percentage = 91.25;
+console.log("percentage:"+percentage);
+
+//2. string
+
+let name1 = "ganesh";
+let name2 ="kumar";
+console.log(name1+name2);
+
+//3. Boolean
+
+let doYouHavePassport = true;
+console.log(doYouHavePassport);
+
+//4. Null
+let empName = null;
+console.log(empName);
+
+//5 . undefimed
+let age;
+console.log(age);
+
+//6. symbol
+
+
